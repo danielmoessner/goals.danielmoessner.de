f5cb3086fee716ecff49062442ac4d1ad600272e
danielmoessner <github@danielmoessner.de>
2025-02-07 22:24:40 +0100

    use uv

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ cd /home/goals.danielmoessner.de/ || exit
 curl -LsSf https://astral.sh/uv/install.sh | sh
 git reset --hard HEAD
 git pull
-tmp/venv/bin/pip install -r requirements.txt
-tmp/venv/bin/python manage.py migrate
-tmp/venv/bin/python manage.py collectstatic --noinput
+uv sync
+uv run manage.py migrate
+uv run manage.py collectstatic --noinput
 ./permissions.sh
 systemctl restart apache2
