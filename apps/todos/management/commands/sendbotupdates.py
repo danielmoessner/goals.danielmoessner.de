df469f16ba74e4a504f77465459611d6368101e3
danielmoessner <github@danielmoessner.de>
2025-06-19 22:00:23 +0200

    get async working

--- apps/todos/management/commands/sendbotupdates.py
+++ apps/todos/management/commands/sendbotupdates.py
@@ -8,8 +8,12 @@ from apps.todos.models import Page
 class Command(BaseCommand):
     help = "Test bot command"
 
-    def handle(self, *args, **options):
-        pages = Page.objects.all()
+    async def async_handle(self, pages: list[Page]):
         for page in pages:
             if page.can_send_updates():
-                asyncio.run(page.send_updates())
+                await page.send_updates()
+
+    def handle(self, *args, **options):
+        pages = Page.objects.all()
+        asyncio.run(self.async_handle(list(pages)))
+        self.stdout.write(self.style.SUCCESS("bot updates sent"))
