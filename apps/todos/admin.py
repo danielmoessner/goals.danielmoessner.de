3f543b5b513971b732a1d0ead733f22d002a9710
danielmoessner <github@danielmoessner.de>
2025-10-29 13:15:30 +0100

    add to admin

--- apps/todos/admin.py
+++ apps/todos/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from apps.todos.models import NeverEndingTodo, NormalTodo, PipelineTodo, RepetitiveTodo
+from apps.todos.models import (
+    NeverEndingTodo,
+    NormalTodo,
+    NotesTodo,
+    Page,
+    PipelineTodo,
+    RepetitiveTodo,
+)
 
 admin.site.register(NormalTodo)
 admin.site.register(NeverEndingTodo)
 admin.site.register(PipelineTodo)
 admin.site.register(RepetitiveTodo)
+admin.site.register(NotesTodo)
+admin.site.register(Page)
